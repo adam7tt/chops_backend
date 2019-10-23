0aae76ab2514d5547cce5d2f74d3548170fb0b76
Adam <adam7tt@gmail.com>
2019-10-23 14:41:45 -0700

    make it better

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Citations)
+admin.site.register(Keywords)
+admin.site.register(KeywordsCitations)
+admin.site.register(Academics)
+admin.site.register(AcademicCitations)
+# admin.site.register(Users)
+admin.site.register(University)
+admin.site.register(Schools)
+admin.site.register(SchoolsAcademics)
+admin.site.register(Departments)
