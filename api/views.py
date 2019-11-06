b34718d60ee9879ed78a24c5b2992a1d4b09deed
nvarg <natvargas97@gmail.com>
2019-11-06 13:55:55 -0800

    dev

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework import mixins
 from .serializers import AcademicSerializer, CitationSerializer, KeywordSerializer, UniversitySerializer, DepartmentSerializer
-from .models import Academic, Citation, Keyword, University, Department
+from .models import *
+from .filters import *
 
 # Create your views here.
 
@@ -11,8 +13,27 @@ def home(request):
     return render(request, 'api/home.html')
 
 class AcademicListAPIView(mixins.CreateModelMixin, generics.ListAPIView):
-    queryset = Academic.objects.all()
     serializer_class = AcademicSerializer
+    filter_class = AcademicFilter
+    filter_backends = [DjangoFilterBackend]
+
+#    def get_queryset(self):
+#        queryset = Academic.objects.all()
+#
+#        kwargs = dict()
+#        kwargs['name'] = self.request.query_params.get('professor', None)
+#        kwargs['university__name'] = self.request.query_params.get('university', None)
+#        kwargs['department__name'] = self.request.query_params.get('department', None)
+#        kwargs['citations__keywords__name'] = self.request.query_params.getlist('keywords', None)
+#
+#        keywords=kwargs['citations__keywords__name']
+#        print(type(keywords), keywords)
+#
+#        for query in kwargs.items():
+#            if query[1] is not None:
+#                queryset = queryset.filter(**dict([query]))
+#
+#        return queryset
 
     def perform_create(self, serializer):
         serializer.save()
