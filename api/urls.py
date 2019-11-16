25fa2ed047767ae04b97a14999046e999f8c37a8
Natan Vargas <33994997+nvarg@users.noreply.github.com>
2019-11-15 16:19:54 -0800

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView
+from .views import *
 
 urlpatterns = {
-    url(r'^citations/$', CreateView.as_view(), name="create"),
+    url(r'^academics/$', AcademicListAPIView.as_view(), name="create"),
+    url(r'^citations/$', CitationListAPIView.as_view(), name="create"),
+    url(r'^keywords/$', KeywordListAPIView.as_view(), name="create"),
+    url(r'^universities/$', UniversityListAPIView.as_view(), name="create"),
+    url(r'^departments/$', DepartmentListAPIView.as_view(), name="create"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
