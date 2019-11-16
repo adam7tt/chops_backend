d863a115b3708213e94485925523b9a6ef74cfc1
Natan Vargas <33994997+nvarg@users.noreply.github.com>
2019-11-15 16:15:16 -0800

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,14 +1,12 @@
+Learn more or give us feedback
 from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Citations)
-admin.site.register(Keywords)
-admin.site.register(KeywordsCitations)
-admin.site.register(Academics)
-admin.site.register(AcademicCitations)
-# admin.site.register(Users)
+admin.site.register(Academic)
+admin.site.register(Citation)
+admin.site.register(Keyword)
 admin.site.register(University)
-admin.site.register(Schools)
-admin.site.register(SchoolsAcademics)
-admin.site.register(Departments)
+admin.site.register(Department)
+# admin.site.register(Users)
+#admin.site.register(School)
